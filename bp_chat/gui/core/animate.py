fb21464514afb3118074054100d402ff54762555
wind <wind@compas.ru>
2019-09-23 11:08:05 +0300

    animate dialog goods

--- bp_chat/gui/core/animate.py
+++ bp_chat/gui/core/animate.py
@@ -1,6 +1,15 @@
 
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton
-from PyQt5.QtCore import QPropertyAnimation, QAbstractAnimation, QRect, QParallelAnimationGroup
+from PyQt5.QtCore import QPropertyAnimation, QAbstractAnimation, QRect, QParallelAnimationGroup, Qt
+
+
+def fix_window(window):
+    flags = window.windowFlags()
+    flags = int(flags)
+    flags &= ~Qt.WindowContextHelpButtonHint
+    flags = Qt.WindowFlags(flags)
+    window.setWindowFlags(flags)
+    return window
 
 
 class AnimatedDialog(QDialog):
@@ -10,16 +19,33 @@ class AnimatedDialog(QDialog):
 
         duration = 500
 
+        parent = self.parentWidget()
+        if parent:
+            pos = parent.pos()
+            size = parent.size()
+            center = (pos.x() + size.width()/2, pos.y() + size.height()/2)
+            parent.setWindowOpacity(0.5)
+        else:
+            desktop = QApplication.desktop()
+            if desktop:
+                rect = QApplication.desktop().screenGeometry()
+                center = (rect.width()/2, rect.height()/2)
+            else:
+                center = (100, 100)
+
         anim1 = QPropertyAnimation(self, b"windowOpacity")
         anim1.setStartValue(0.0)
         anim1.setEndValue(1.0)
         anim1.setDuration(duration)
 
+        w, h = self.width(), self.height()
+        w2, h2 = w * 2, h * 2
+
         anim2 = QPropertyAnimation(self, b"geometry")
         anim2.setDuration(duration)
-        anim2.setKeyValueAt(0, QRect(100, 100, 200, 200))
+        anim2.setKeyValueAt(0, QRect(center[0]-w/2, center[1]-h/2, w, h))
         #anim2.setKeyValueAt(0.8, QRect(250, 250, 100, 30))
-        anim2.setKeyValueAt(1, QRect(100, 100, 500, 300))
+        anim2.setKeyValueAt(1, QRect(center[0]-w2/2, center[1]-h2/2, w2, h2))
 
         self.group = QParallelAnimationGroup(self)
         self.group.addAnimation(anim1)
@@ -27,15 +53,25 @@ class AnimatedDialog(QDialog):
 
         self.group.start(QAbstractAnimation.DeleteWhenStopped)
 
+    def hideEvent(self, event):
+        super().hideEvent(event)
+
+        parent = self.parentWidget()
+        if parent:
+            parent.setWindowOpacity(1)
+
+
 
 if __name__=='__main__':
 
     app = QApplication([])
 
-    w = QPushButton("Test")
+    w = fix_window(QPushButton("Test"))
+    w.resize(800, 800)
 
     def _show(*args):
-        w.a = AnimatedDialog(w)
+        w.a = fix_window(AnimatedDialog(w))
+        w.a.resize(200, 200)
         w.a.exec_()
 
     w.clicked.connect(_show)
